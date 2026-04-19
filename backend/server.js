b5d314c36b731dcc1db927768249b75b781f6bfa
IrbadAbdeldjelil <irbadabdeldjelil@gmail.com>
2026-04-19 09:35:09 +0200

    create the db and conneted to it

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
 require('dotenv').config();
+const sequelize = require('./src/config/db.config')
 const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`the server is listening on${PORT}`));
+async function start() {
+    // connet to db
+    try {
+        await sequelize.authenticate();
+        console.log('connect to DB successfully');
+        await sequelize.sync();
+        console.log('table synced');
+        app.listen(PORT, () => console.log(`the server is listening on${PORT}`));
+
+    } catch (err) {
+        console.log('DB connection errors', err);
+        process.exit(1);
+        
+    }
+}
+// start server
+start()
